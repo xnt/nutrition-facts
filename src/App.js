1d4b5616bc529d6076e4d677a8faee0598f6b9da
Vicente Plata <xientyto@gmail.com>
2020-06-13 22:34:18 -0400

    PropTypes validation, formatting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import NutritionFacts from "./NutritionFacts";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <NutritionFacts />
+      <NutritionFacts productName="Pizza" servingSize={{ amount: 20, dimension: "g" }} />
     </div>
   );
 }
--- src/NutritionFacts.js
+++ src/NutritionFacts.js
@@ -1,5 +1,56 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Container, Row, Col } from "react-bootstrap";
 
-const NutritionFacts = () => <div>Nutrition Facts placeholder</div>;
+const numberPercentageShape = PropTypes.shape({
+  number: PropTypes.number,
+  percentage: PropTypes.number,
+});
+
+const NutritionFacts = ({ productName, servingSize }) => (
+  <div>
+    <Container>
+      <Row>
+        <Col>
+          <h2>{productName} Nutrition Facts</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>Serving Size</Col>
+        <Col>
+          {servingSize.amount} {servingSize.dimension}
+        </Col>
+      </Row>
+    </Container>
+  </div>
+);
+
+NutritionFacts.propTypes = {
+  productName: PropTypes.string.isRequired,
+  servingSize: PropTypes.shape({
+    amount: PropTypes.number,
+    dimension: PropTypes.string,
+  }).isRequired,
+  calories: PropTypes.number.isRequired,
+  fat: PropTypes.shape({
+    saturated: numberPercentageShape,
+    trans: PropTypes.number,
+    total: numberPercentageShape,
+  }),
+  cholesterol: numberPercentageShape,
+  sodium: numberPercentageShape,
+  carbs: PropTypes.shape({
+    total: numberPercentageShape,
+    fiber: numberPercentageShape,
+    sugars: PropTypes.number,
+  }),
+  protein: PropTypes.number,
+  vitamins: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      amount: numberPercentageShape,
+    })
+  ),
+};
 
 export default NutritionFacts;
